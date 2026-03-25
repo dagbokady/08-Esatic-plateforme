4b8a35ce019bd97dc182fd2031f48c756bec575b
dagbokady <dagbokady@gmail>
2026-03-25 08:34:42 +0000

    chore: run migrations and seed on startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
-
+from alembic.config import Config
+from alembic import command
+from app.database import engine
+from app.models import *
 load_dotenv()
 
 ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
@@ -33,6 +36,27 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(classes.router)
 app.include_router(files.router)
+
+
+
+def run_migrations():
+    try:
+        alembic_cfg = Config("alembic.ini")
+        command.upgrade(alembic_cfg, "head")
+        print("✅ Migrations OK")
+    except Exception as e:
+        print(f"⚠️ Migrations : {e}")
+
+def run_seed():
+    from app.utils.seed import seed
+    try:
+        seed()
+    except Exception as e:
+        print(f"⚠️ Seed : {e}")
+
+
+run_migrations()
+run_seed()
 # ── ROUTES DE BASE ────────────────────────────────────
 @app.get("/")
 def root():
